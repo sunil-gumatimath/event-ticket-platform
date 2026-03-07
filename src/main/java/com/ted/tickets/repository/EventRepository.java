aa5842187094ca34b36eb5513de906e088cb9dbf
SuniL <sunilgumatimath.vs@gmail.com>
2026-03-08 05:03:20 +0530

    feat: add repositories

---
+++ src/main/java/com/ted/tickets/repository/EventRepository.java
@@ -0,0 +1,17 @@
+package com.ted.tickets.repository;
+
+import com.ted.tickets.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
+    Optional<Event> findByIdAndOrganizerId(UUID id, UUID organizerId);
+}
---
+++ src/main/java/com/ted/tickets/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ted.tickets.repository;
+
+import com.ted.tickets.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
