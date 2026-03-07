c9441a81118602fe78f27626ebcd22f50dfe1ae4
SuniL <sunilgumatimath.vs@gmail.com>
2026-03-08 05:03:20 +0530

    feat: add event mapper

---
+++ src/main/java/com/ted/tickets/mappers/EventMapper.java
@@ -0,0 +1,28 @@
+package com.ted.tickets.mappers;
+
+import com.ted.tickets.dto.response.ListEventResponseDto;
+import com.ted.tickets.dto.response.ListEventTicketTypeResponseDto;
+import com.ted.tickets.entity.Event;
+import com.ted.tickets.dto.request.CreateEventRequestDto;
+import com.ted.tickets.dto.request.CreateTicketTypeRequestDto;
+import com.ted.tickets.dto.response.CreateEventResponseDto;
+import com.ted.tickets.domain.model.CreateEventRequest;
+import com.ted.tickets.domain.model.CreateTicketTypeRequest;
+import com.ted.tickets.entity.TicketType;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface EventMapper {
+
+    CreateTicketTypeRequest fromDto(CreateTicketTypeRequestDto dto);
+
+    CreateEventRequest fromDto(CreateEventRequestDto dto);
+
+    CreateEventResponseDto toDto(Event event);
+
+    ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+    ListEventResponseDto toListEventResponseDto (Event event);
+
+}
