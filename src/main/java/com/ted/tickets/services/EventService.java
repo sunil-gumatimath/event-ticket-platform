c19839b2c331c6c61bfca79595ecb9271025b766
SuniL <sunilgumatimath.vs@gmail.com>
2026-03-08 05:03:20 +0530

    feat: add event service interface

---
+++ src/main/java/com/ted/tickets/services/EventService.java
@@ -0,0 +1,15 @@
+package com.ted.tickets.services;
+
+import com.ted.tickets.domain.model.CreateEventRequest;
+import com.ted.tickets.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface EventService {
+    Event createEvent(UUID organizerId, CreateEventRequest event);
+    Page<Event> listEventsForOrganizer(UUID organizerId , Pageable pageable);
+    Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
+}
