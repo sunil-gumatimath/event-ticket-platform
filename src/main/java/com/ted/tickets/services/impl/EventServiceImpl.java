f1f44dacb0d7ff4a3fc19a9d9447eda2ba187e29
SuniL <sunilgumatimath.vs@gmail.com>
2026-03-08 05:03:20 +0530

    feat: add event service implementation

---
+++ src/main/java/com/ted/tickets/services/impl/EventServiceImpl.java
@@ -0,0 +1,70 @@
+package com.ted.tickets.services.impl;
+
+import com.ted.tickets.domain.model.CreateEventRequest;
+import com.ted.tickets.entity.Event;
+import com.ted.tickets.entity.TicketType;
+import com.ted.tickets.entity.User;
+import com.ted.tickets.exceptions.UserNotFoundException;
+import com.ted.tickets.repository.EventRepository;
+import com.ted.tickets.repository.UserRepository;
+import com.ted.tickets.services.EventService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+
+    private  final UserRepository userRepository;
+    private  final EventRepository eventRepository;
+
+
+    @Override
+    public Event createEvent(UUID organizerId, CreateEventRequest event) {
+        User organizer =userRepository.findById(organizerId).orElseThrow(
+                ()-> new UserNotFoundException(String.format("User not found with id " + organizerId))
+        );
+
+        List<TicketType> ticketTypes = event.getTicketTypes().stream().map(
+                ticketType -> {
+                    TicketType ticketTypeToCreate = new TicketType();
+                    ticketTypeToCreate.setName(ticketType.getName());
+                    ticketTypeToCreate.setPrice(ticketType.getPrice());
+                    ticketTypeToCreate.setDescription(ticketType.getDescription());
+                    ticketTypeToCreate.setTotalAvailable(ticketType.getTotalAvailable());
+
+                    return ticketTypeToCreate;
+
+                }).toList();
+
+        Event eventToCreate = new Event();
+        eventToCreate.setName(event.getName());
+        eventToCreate.setStart(event.getStart());
+        eventToCreate.setEnd(event.getEnd());
+        eventToCreate.setVenue(event.getVenue());
+        eventToCreate.setSalesStart(event.getSalesStart());
+        eventToCreate.setSalesEnd(event.getSalesEnd());
+        eventToCreate.setStatus(event.getStatus());
+        eventToCreate.setOrganizer(organizer);
+        eventToCreate.setTicketTypes(ticketTypes);
+
+        return eventRepository.save(eventToCreate);
+    }
+
+    @Override
+    public Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable) {
+        return eventRepository.findByOrganizerId(organizerId, pageable);
+    }
+
+    @Override
+    public Optional<Event> getEventForOrganizer(UUID organizerId, UUID id) {
+        return eventRepository.findByIdAndOrganizerId(id , organizerId);
+    }
+}
