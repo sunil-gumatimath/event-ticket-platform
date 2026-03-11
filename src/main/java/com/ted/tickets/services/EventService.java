c0fd32ce3ea6651555877176fd7a95f3d7c57e51
SuniL <sunilgumatimath.vs@gmail.com>
2026-03-12 01:14:47 +0530

    Add functionality to list published events

--- src/main/java/com/ted/tickets/services/EventService.java
+++ src/main/java/com/ted/tickets/services/EventService.java
@@ -15,4 +15,5 @@ public interface EventService {
     Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
     Event updateEventForOrganizer(UUID organizerId, UUID id, UpdateEventRequest event);
     void deleteEventForOrganizer(UUID organizerId, UUID id);
+    Page<Event> listPublishedEvents(Pageable pageable);
 }
